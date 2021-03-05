840eb6fd444faf7fcb76fad9faf9132a06e820ed
dutlzn <2436013662@qq.com>
2021-03-05 22:11:09 +0800

    densenet

---
+++ Pytorch/DenseNet.py
@@ -0,0 +1,13 @@
+'''
+注意DenseNet中各种维度变化的过程
+'''
+class TestClass():
+    def __init__(self):
+        print("类初始化")
+    
+
+    def _printHello(self):
+        print("hello")
+
+tc = TestClass()
+tc._printHello()
